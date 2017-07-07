6ba9f849917c631b1004086d40f7924c72adcb93
LoganAFBuchanan <Loganbuchanan@hotmail.com>
2017-07-06 21:43:27 -0700

    Added First session events
    Also added animations to player cards and timeline entries

--- timelines/timelinesscript.js
+++ timelines/timelinesscript.js
@@ -1,17 +1,78 @@
 
 
+
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function(partial) {
+
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+
+})(jQuery);
+
 function recalc(){
   $(".tree-straight").each(function (i) {
     $(this).height($(this).parent().css("height"));
   });
 }
 
+var win = $(window);
+var allMods = $("blockquote");
+
+// Already visible modules
+allMods.each(function(i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+win.scroll(function(event) {
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("slide-up");
+    }else{
+      el.removeClass("slide-up");
+    }
+  });
+});
+
+//Animates blockquotes when they scroll into view
 
 
 $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
   recalc();
-})
 
+
+  allMods.each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("already-visible");
+    }
+  });
+})
 
 $( document ).ready(function() {
 
